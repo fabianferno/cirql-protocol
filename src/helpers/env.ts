d0dcc550a54d256c205aad4becb58fb5fdd9ca0c
Fabian Ferno <fabianferno@gmail.com>
2024-01-20 15:06:59 +0530

    Clean env

---
+++ src/helpers/env.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv'
+import { cleanEnv, num, str } from 'envalid'
+import { cwd } from 'process'
+import { resolve } from 'path'
+
+dotenv.config({ path: resolve(cwd(), '.env') })
+
+// eslint-disable-next-line node/no-process-env
+export default cleanEnv(process.env, {
+  MONGO: str(),
+  PORT: num({ default: 1337 }),
+  OPENAI_API_KEY: str(),
+  FARCASTER_MNEMONIC: str(),
+  BOT_FNAME: str({
+    default: 'karmabot',
+  }),
+})
