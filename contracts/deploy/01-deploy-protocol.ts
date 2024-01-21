e4510b7cd57770a49e792fb24434613b42921ffa
Fabian Ferno <fabianferno@gmail.com>
2024-01-21 18:16:06 +0530

    Create deploy script

---
+++ contracts/deploy/01-deploy-protocol.ts
@@ -0,0 +1,55 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import verify from "../helper-functions"
+import { parseEther } from "ethers/lib/utils"
+import { AaveV3Sepolia } from "@bgd-labs/aave-address-book" // import specific pool
+import { networkConfig, developmentChains } from "../helper-hardhat-config"
+
+const deployProtocol: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  // @ts-ignore
+  const { getNamedAccounts, deployments, network } = hre
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  log("----------------------------------------------------")
+  log("Deploying Protocol and waiting for confirmations...")
+
+  // Deploy Cirql Token
+  const CirqlToken = await deploy("CirqlToken", {
+    from: deployer,
+    args: [],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: networkConfig[network.name]?.blockConfirmations || 1,
+  })
+  log(`CirqlToken at ${CirqlToken.address}`)
+
+  // GHO token contract
+  let GHO_Token = "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60"
+  // let daiToken = "0xFF34B3d4Aee8ddCd6F9AFFFB6Fe49bD371b8a357"
+  let pool = AaveV3Sepolia.POOL // 0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951
+  const Contract = await deploy("CirqlProtocol", {
+    from: deployer,
+    args: [CirqlToken.address, pool, GHO_Token],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: networkConfig[network.name]?.blockConfirmations || 1,
+  })
+  log(`Contract at ${Contract.address}`)
+
+  // Verifications
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    // Wait for 5 block confirmations
+    log("Waiting for 5 block confirmations before verifying...")
+    await hre.ethers.provider.waitForTransaction(Contract.transactionHash || "", 5)
+
+    await verify(Contract.address, network.name, [
+      // args
+      CirqlToken.address,
+      pool,
+      GHO_Token,
+    ])
+  }
+}
+
+export default deployProtocol
+deployProtocol.tags = ["all", "Protocol"]
